6cd1acb923e028a55b7011004c23e20745f83731
Emon_Shikder <emonshikder2217@gmail.com>
2025-04-06 14:46:12 +0600

    initial commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,21 +1,30 @@
 import React from "react";
 import "./Hero.css";
 import heroImg from '../../assets/hero_img.png';
+import "@fontsource/geist-mono";
+
 
 const Hero = () => {
+
+
+  const style = {
+    fontFamily: `"Geist Mono", "Geist Mono Fallback", monospace`,
+  };
+
+
   return (
     <section className="hero_section">
       <div class="overlay">
         <div className="hero_container">
-          <h1 className="hero_title">
+          <h1 className="hero_title" style={style}>
             Mass English
           </h1>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing </p>
+          <p className="subtitle">Best English Learning  Coaching Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas odio accusantium dolore, est vel facere recusandae non voluptatibus! Voluptate, provident quisquam eligendi voluptas porro dolore?</p>
           <button>Courses</button>
         </div>
       </div>
-      
-      
+
+
     </section>
   );
 };
