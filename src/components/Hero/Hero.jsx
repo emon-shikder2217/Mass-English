de678da9a06d2ef8d60d4e2a06879a6945e1358a
Emon_Shikder <emonshikder2217@gmail.com>
2025-04-06 16:59:30 +0600

    initial commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Hero.css";
 import heroImg from '../../assets/hero_img.png';
 import "@fontsource/geist-mono";
@@ -6,6 +6,14 @@ import "@fontsource/geist-mono";
 
 const Hero = () => {
 
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    setMousePosition({ x, y });
+  };
 
   const style = {
     fontFamily: `"Geist Mono", "Geist Mono Fallback", monospace`,
@@ -14,15 +22,22 @@ const Hero = () => {
 
   return (
     <section className="hero_section">
-      <div class="overlay">
-        <div className="hero_container">
-          <h1 className="hero_title" style={style}>
-            Mass English
-          </h1>
-          <p className="subtitle">Best English Learning  Coaching Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas odio accusantium dolore, est vel facere recusandae non voluptatibus! Voluptate, provident quisquam eligendi voluptas porro dolore?</p>
-          <button>Courses</button>
+      <div class="overlay"></div>
+      <div className="hero_container">
+        <h1 className="hero_title" style={style}>
+          Mass English
+        </h1>
+        <p className="subtitle">Best English Learning  Coaching Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas odio accusantium dolore, est vel facere recusandae non voluptatibus! Voluptate, provident quisquam eligendi voluptas porro dolore?</p>
+
+        <div className="container">
+          <button
+            className="button"
+          >
+            View Courses
+          </button>
         </div>
       </div>
+
 
 
     </section>
