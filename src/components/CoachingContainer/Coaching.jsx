a5724c413aefca879533e934dc0d10a80845ae59
Emon_Shikder <emonshikder2217@gmail.com>
2025-04-20 18:20:44 +0600

    initial commit

---
+++ src/components/CoachingContainer/Coaching.jsx
@@ -0,0 +1,9 @@
+import './Coaching.css';
+
+
+const Coaching = () => {
+  return (
+    <div className="aboutCoaching"></div>
+  )
+}
+export default Coaching;
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,29 +1,15 @@
 import "./About.css";
-import { BsBoxArrowLeft } from "react-icons/bs";
-import { Link } from "react-router-dom";
-
+import Coaching from "../../components/CoachingContainer/Coaching";
+import Tutor from "../../components/AboutTutor/Tutor";
 
 const About = () => {
   return (
+    <>
     <div className="about-container">
-      <div className="about-overlay"></div>
-      <Link to="/" className="backArrowAbout">
-        <BsBoxArrowLeft />
-      </Link>
-      <div className="aboutTutor">
-        <div className="tutorTitle">
-          <h1 className="tutorHeading">About Tutor</h1>
-        </div>
-
-        <div className="tutorInfo">
-          <div className="tutorImg">
-            <img src="" alt="" />
-          </div>
-          <div className="tutorTxt"></div>
-        </div>
-      </div>
-      <div className="aboutCoaching"></div>
+      <Tutor />
+      <Coaching />
     </div>
+    </>
   );
 };
 export default About;
