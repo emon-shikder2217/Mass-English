e32dfa4200314cae25aa754302307357e2199e4b
Emon_Shikder <emonshikder2217@gmail.com>
2025-05-11 17:25:10 +0600

    initial commit

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -3,37 +3,44 @@ import './ContactForm.css';
 const ContactForm = () => {
   return (
     <>
-  
-
-      <form
-        className="contactForm"
-      >
-        <div className="name">
-          <input className="name_input" type="text" name="name" required />
-          <div className="labelline">Enter Your Name</div>
-        </div>
-        <div className="email">
-          <input className="email_input" type="email" name="email" required />
-          <div className="label_line">Enter Your Email</div>
-        </div>
-        <div className="textarea">
-          <textarea
-            className="message_input"
-            name="message"
-            type="text"
-            required
-          />
-          <div className="label_line_msg">Enter Your Message</div>
-        </div>
-        <div className="button_container">
-          <button className="contact_btn" type="submit">
-            Submit
-          </button>
-        </div>
-      </form>
-     
+      <div className="contactContainerr">
+        <form className="contactForm">
+          <div className="name">
+            <input
+              className="name_input"
+              type="text"
+              name="name"
+              required
+              placeholder="Enter Your Name"
+            />
+          </div>
+          <div className="email">
+            <input
+              className="email_input"
+              type="email"
+              name="email"
+              required
+              placeholder="Enter Your Email"
+            />
+          </div>
+          <div className="textarea">
+            <textarea
+              className="message_input"
+              name="message"
+              type="text"
+              required
+              placeholder="Enter Your Message"
+            />
+          </div>
+          <div className="button_container">
+            <button className="contact_btn" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </>
-  )
-}
-export default ContactForm;
+  );
+};
 
+export default ContactForm;
